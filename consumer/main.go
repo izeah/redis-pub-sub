41c760d3a97869ed3ef633ce2e7ba46a324182e8
Faiz Mohammad Hafidza <faizmohhafidza@gmail.com>
2023-12-18 10:54:33 +0700

    update pubsub

--- consumer/main.go
+++ consumer/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,42 +11,60 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-var RedisClient = redis.NewClient(&redis.Options{
-	Addr: "localhost:6379",
-})
+var redisDB *redis.Client
+
+type Factory struct {
+	RedisDB *redis.Client
+}
 
 func main() {
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, os.Interrupt)
 	signal.Notify(shutdown, syscall.SIGTERM)
 
-	RegisterConsumer(shutdown, RedisClient)
+	RedisInit()
+	defer RedisClose()
 
-	<-shutdown
+	f := &Factory{
+		RedisDB: redisDB,
+	}
 
-	fmt.Println("shutdown")
+	RegisterConsumer(shutdown, f.RedisDB)
+}
+
+func RedisInit() {
+	redisDB = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	if msg := redisDB.Ping(context.TODO()).Val(); msg != "PONG" {
+		panic("failed to connect to redis")
+	}
+	log.Println("successfully connect to redis")
+}
+
+func RedisClose() {
+	redisDB.Close()
+	log.Println("redis connection is closed")
 }
 
 func RegisterConsumer(shutdown chan os.Signal, redisDB *redis.Client) {
 	fmt.Println("starting Consumer ...")
-	sub := redisDB.Subscribe(context.TODO(), "data_baru")
-	defer func() {
-		if sub != nil {
-			_ = sub.Close()
-		}
-	}()
+	pubsub := redisDB.Subscribe(context.TODO(), "data_baru")
+	if _, err := pubsub.Receive(context.TODO()); err != nil {
+		panic("failed to connect to redis subscriber")
+	}
+	ch := pubsub.Channel()
 	for {
 		select {
+		case msg := <-ch:
+			if msg != nil {
+				fmt.Println("consumed data:", msg.Payload)
+			}
 		case <-shutdown:
+			_ = pubsub.Close()
+			fmt.Println("Pubsub closed. Channel closed.")
 			fmt.Println("shutdown signal received, exiting ...")
 			return
-		default:
-			msg, err := sub.ReceiveMessage(context.TODO())
-			if err != nil {
-				fmt.Println("error occured, because:", err.Error())
-				continue
-			}
-			fmt.Println("consumed data:", msg.Payload)
 		}
 	}
 }
--- producer/main.go
+++ producer/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,26 +12,46 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-var RedisClient = redis.NewClient(&redis.Options{
-	Addr: "localhost:6379",
-})
+var redisDB *redis.Client
+
+type Factory struct {
+	RedisDB *redis.Client
+}
 
 func main() {
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, os.Interrupt)
 	signal.Notify(shutdown, syscall.SIGTERM)
 
-	RegisterProducer(shutdown, RedisClient)
+	RedisInit()
+	defer RedisClose()
 
-	<-shutdown
+	f := &Factory{
+		RedisDB: redisDB,
+	}
 
-	fmt.Println("shutdown")
+	RegisterProducer(shutdown, f.RedisDB)
+}
+
+func RedisInit() {
+	redisDB = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	if msg := redisDB.Ping(context.Background()).Val(); msg != "PONG" {
+		panic("failed to connect to redis")
+	}
+	log.Println("successfully connect to redis")
+}
+
+func RedisClose() {
+	redisDB.Close()
+	log.Println("redis connection is closed")
 }
 
 func RegisterProducer(shutdown chan os.Signal, redisDB *redis.Client) {
 	counter := 0
 	fmt.Println("starting producer ...")
-	ticker := time.NewTicker(60 * time.Second)
+	ticker := time.NewTicker(3 * time.Second)
 	for {
 		select {
 		case <-shutdown:
